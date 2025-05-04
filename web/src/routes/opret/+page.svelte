c684b6e2c88e4d86a9bda347b943b8c3b460022d
anvidev <andreasgylche@gmail.com>
2025-05-04 10:35:46 +0200

    feat: started create form

--- web/src/routes/opret/+page.svelte
+++ web/src/routes/opret/+page.svelte
@@ -1,38 +1,42 @@
 <script lang="ts">
 	import { Label } from '$lib/components/ui/label/index.js'
+	import { Checkbox } from '$lib/components/ui/checkbox/index.js'
 	import { Input } from '$lib/components/ui/input/index.js'
 	import { Button } from '$lib/components/ui/button/index.js'
 	import SuperDebug from 'sveltekit-superforms'
 	import { superForm } from 'sveltekit-superforms'
 
 	let { data } = $props()
-
+	let isBillingSame = $state(false)
 	const { form } = superForm(data.form)
 
-	function copySenderToBilling() {
-		$form.billingName = $form.senderName
-	}
+	$effect(() => {
+		if (isBillingSame) {
+			$form.billingName = $form.senderName
+		}
+	})
 </script>
 
 <h1>Opret RMA</h1>
 
 <form method="post" class="space-y-4 rounded-xl border p-6">
 	<div class="grid gap-2">
-		<Label for="senderName">Afsender navn</Label>
-		<Input id="senderName" bind:value={$form.senderName} />
+		<Label for="senderName">Afsender firma<span class="text-red-500">*</span></Label>
+		<Input id="senderName" disabled={isBillingSame} bind:value={$form.senderName} />
 	</div>
 
-	<Button type="button" variant="outline" class="mb-4" onclick={copySenderToBilling}
-		>Samme som afsender</Button
-	>
-
-	<div class="grid gap-2">
-		<Label for="billingName">Fakturerings navn</Label>
-		<Input id="billingName" bind:value={$form.billingName} />
+	<div class="flex items-center gap-2">
+		<Checkbox id="billing-checkbox" bind:checked={isBillingSame} />
+		<Label for="billing-checkbox">Fakturering er den samme som afsender</Label>
 	</div>
 
 	<div class="grid gap-2">
-		<Label for="categorties">Kaegorier</Label>
+		<Label for="billingName">Fakturerings firma<span class="text-red-500">*</span></Label>
+		<Input id="billingName" disabled={isBillingSame} bind:value={$form.billingName} />
+	</div>
+
+	<div class="grid gap-2">
+		<Label for="categorties">Kategorier<span class="text-red-500">*</span></Label>
 		<div>
 			{#each ['software', 'hardware'] as category}
 				<label>
