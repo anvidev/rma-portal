54ec783c54db3c7333e69d31feb2fd51eb685aaf
anvidev <andreasgylche@gmail.com>
2025-05-19 10:26:06 +0200

    feat: simple navigation links

--- web/src/routes/(site)/+layout.svelte
+++ web/src/routes/(site)/+layout.svelte
@@ -7,17 +7,25 @@
 	let logoAnchorPath = $derived(data.user ? '/admin/tickets' : '/opret')
 </script>
 
-<main class="bg-muted/20 relative min-h-screen w-full">
+<main class="relative min-h-screen w-full bg-muted/20">
 	<header
 		class="sticky top-0 flex items-center justify-between border-b bg-white px-3 py-4 shadow-sm"
 	>
-		<a href={logoAnchorPath} class="flex items-center gap-2 font-medium">
-			<div class="flex h-[30px] items-center gap-1 rounded-lg bg-red-500 px-2 text-base text-white">
-				<ScanBarcode class="size-[18px]" />
-				<span class="leading-none">Skancode</span>
-			</div>
-			<span class="text-base">RMA Service Portal</span>
-		</a>
+		<div class="flex items-center gap-8">
+			<a href={logoAnchorPath} class="flex items-center gap-2 font-medium">
+				<div
+					class="flex h-[30px] items-center gap-1 rounded-lg bg-red-500 px-2 text-base text-white"
+				>
+					<ScanBarcode class="size-[18px]" />
+					<span class="leading-none">Skancode</span>
+				</div>
+				<span class="text-base">RMA Service Portal</span>
+			</a>
+			<nav class="flex items-center gap-4 text-sm text-muted-foreground">
+				<a class="hover:underline" href="/admin/tickets">Sags oversigt</a>
+				<a class="hover:underline" href="/opret">Opret RMA</a>
+			</nav>
+		</div>
 		{#if data.user}
 			<UserButton user={data.user} />
 		{/if}
