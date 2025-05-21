86e0c4eca815343375ec8cc9019b2a172dc7675a
anvidev <andreasgylche@gmail.com>
2025-05-21 11:17:03 +0200

    feat: add login button to header

--- web/src/routes/(site)/+layout.svelte
+++ web/src/routes/(site)/+layout.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
 	import { page } from '$app/state'
 	import UserButton from '$lib/components/common/user-button.svelte'
+	import Button from '$lib/components/ui/button/button.svelte'
 	import { cn } from '$lib/utils.js'
 	import { ScanBarcode } from '@lucide/svelte'
 
 	let { children, data } = $props()
 </script>
 
-<main class="relative flex min-h-screen w-full flex-col bg-muted/20">
+<main class="bg-muted/20 relative flex min-h-screen w-full flex-col">
 	<header
 		class="sticky top-4 mx-auto mt-4 flex w-full max-w-6xl items-center justify-between rounded-lg border-b bg-white px-3 py-2 shadow-sm"
 	>
@@ -19,7 +20,7 @@
 				<span class="text-sm leading-none">Skancode RMA</span>
 			</a>
 			{#if data.user}
-				<nav class="flex items-center gap-4 text-sm font-medium text-muted-foreground">
+				<nav class="text-muted-foreground flex items-center gap-4 text-sm font-medium">
 					<a
 						class={cn(page.url.pathname == '/admin/tickets' && 'text-foreground')}
 						href="/admin/tickets">Oversigt</a
@@ -30,6 +31,8 @@
 		</div>
 		{#if data.user}
 			<UserButton user={data.user} />
+		{:else}
+			<Button href="/log-ind" size="sm" variant="secondary" class="h-7">Log ind</Button>
 		{/if}
 	</header>
 	<div class="mx-auto flex w-full max-w-6xl flex-1 flex-col px-3 py-4">
