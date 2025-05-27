1887d1e66be3ac713c0fe1717723a6b518609211
anvidev <andreasgylche@gmail.com>
2025-05-27 20:17:46 +0200

    chore: move some util functions to utils

--- web/src/lib/utils.ts
+++ web/src/lib/utils.ts
@@ -14,3 +14,42 @@ export const formatDate = (dateString: string) => {
 		minute: '2-digit',
 	})
 }
+
+export function censor(s: string): string {
+	return s.replaceAll(/./g, '*')
+}
+
+export function censorPhone(s: string): string {
+	let count = 0
+	let result = ''
+
+	for (let i = s.length - 1; i >= 0; i--) {
+		const char = s[i]
+		if (char !== ' ' && /\d/.test(char)) {
+			if (count < 4) {
+				result = char + result
+				count++
+			} else {
+				result = '*' + result
+			}
+		} else {
+			result = char + result
+		}
+	}
+
+	return result
+}
+
+export function censorEmail(email: string): string {
+	const [local, domain] = email.split('@')
+	if (!domain) return email
+
+	if (local.length <= 2) {
+		return '*'.repeat(local.length) + '@' + domain
+	}
+
+	const first = local[0]
+	const last = local[local.length - 1]
+	const stars = '*'.repeat(local.length - 2)
+	return `${first}${stars}${last}@${domain}`
+}
