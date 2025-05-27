8f6f07d750ee2508646a2ed3a9b4d3fabce00eb9
anvidev <andreasgylche@gmail.com>
2025-05-27 14:25:48 +0200

    fix: censor customer information on public track page

--- web/src/routes/(site)/sager/[id]/+page.svelte
+++ web/src/routes/(site)/sager/[id]/+page.svelte
@@ -3,9 +3,38 @@
 	import Details from '$lib/components/tickets/details.svelte'
 	import LogsListExternal from '$lib/components/tickets/logs-list-external.svelte'
 	import Title from '$lib/components/tickets/title.svelte'
+	import type { Contact } from '$lib/types.js'
 
 	let { data } = $props()
 	const ticket = $derived(data.ticket)
+
+	function censor(s: string): string {
+		return s.replaceAll(/./g, '*')
+	}
+
+	const censoredSender: Contact = $derived.by(() => {
+		return {
+			name: censor(ticket.sender.name),
+			phone: censor(ticket.sender.phone),
+			email: censor(ticket.sender.email),
+			street: censor(ticket.sender.street),
+			city: censor(ticket.sender.city),
+			zip: censor(ticket.sender.zip),
+			country: censor(ticket.sender.country),
+		}
+	})
+
+	const censoredBilling: Contact = $derived.by(() => {
+		return {
+			name: censor(ticket.billing.name),
+			phone: censor(ticket.billing.phone),
+			email: censor(ticket.billing.email),
+			street: censor(ticket.billing.street),
+			city: censor(ticket.billing.city),
+			zip: censor(ticket.billing.zip),
+			country: censor(ticket.billing.country),
+		}
+	})
 </script>
 
 <title>RMA Sag #{data.ticket.id} - Skancode RMA Service Portal</title>
@@ -15,8 +44,12 @@
 	<div class="flex flex-col gap-4 md:flex-row">
 		<Details {ticket} />
 		<div class="flex flex-col gap-4 md:w-1/2">
-			<ContactInformation contact={ticket.sender} title="Afsender" sub="Returaddresse" />
-			<ContactInformation contact={ticket.billing} title="Fakturering" sub="Faktureringsaddresse" />
+			<ContactInformation contact={censoredSender} title="Afsender" sub="Returaddresse" />
+			<ContactInformation
+				contact={censoredBilling}
+				title="Fakturering"
+				sub="Faktureringsaddresse"
+			/>
 		</div>
 	</div>
 	<LogsListExternal {ticket} />
