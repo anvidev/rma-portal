ade5790d471ce083541308b0a5512b6b05b2ce1f
anvidev <andreasgylche@gmail.com>
2025-05-27 20:16:54 +0200

    feat: add year 'YY' to ticket id

--- cmd/migrate/migrations/000004_alter_ticket_trigger.up.sql
+++ cmd/migrate/migrations/000004_alter_ticket_trigger.up.sql
@@ -3,7 +3,7 @@ drop trigger if exists trg_generate_ticket_id on tickets;
 create or replace function generate_ticket_id()
 returns trigger as $$
 begin
-    new.id := lpad(new.seq_id::text, 5, '0') || '-' || 
+    new.id := to_char(current_date, 'YY') || lpad(new.seq_id::text, 3, '0') || '-' || 
               upper(substring(md5(random()::text), 1, 10));
     return new;
 end;
