c805d7f65004c286d361a46ab14d46b710c6790c
anvidev <andreasgylche@gmail.com>
2025-05-27 15:29:27 +0200

    feat: tooltip component shadcn

---
+++ web/src/lib/components/ui/tooltip/index.ts
@@ -0,0 +1,21 @@
+import { Tooltip as TooltipPrimitive } from "bits-ui";
+import Content from "./tooltip-content.svelte";
+
+const Root = TooltipPrimitive.Root;
+const Trigger = TooltipPrimitive.Trigger;
+const Provider = TooltipPrimitive.Provider;
+const Arrow = TooltipPrimitive.Arrow
+
+export {
+	Root,
+	Trigger,
+	Content,
+	Provider,
+	Arrow,
+	//
+	Root as Tooltip,
+	Content as TooltipContent,
+	Trigger as TooltipTrigger,
+	Provider as TooltipProvider,
+	Arrow as TooltipArrow
+};
