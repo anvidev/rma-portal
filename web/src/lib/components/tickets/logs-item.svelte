391a9542c08aa2819fd64475cf8f68eab4391c31
anvidev <andreasgylche@gmail.com>
2025-05-27 15:29:49 +0200

    feat: files ui for logs / mocked

--- web/src/lib/components/tickets/logs-item.svelte
+++ web/src/lib/components/tickets/logs-item.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
 	import { type Log } from '$lib/types'
+	import * as Tooltip from '$lib/components/ui/tooltip/index.js'
 	import { cn, formatDate } from '$lib/utils'
-	import { Shield } from '@lucide/svelte'
-	let { log }: { log: Log } = $props()
+	import { FileText, Shield } from '@lucide/svelte'
+	let { log, internal = false }: { log: Log; internal?: boolean } = $props()
 
 	function getStatusBorderColor(status: string): string {
 		switch (status) {
@@ -28,6 +29,21 @@
 				return 'border-l-gray-600'
 		}
 	}
+
+	const attachments = [
+		{
+			id: 1,
+			type: 'image',
+			filename: 'mike.jpg',
+			url: 'https://anvi.dev/files/00007-C43E6/1748259425-mike.jpg',
+		},
+		{
+			id: 2,
+			type: 'document',
+			filename: 'report.pdf',
+			url: 'https://anvi.dev/files/00007-C43E6/bd378640-2bc6-4c9a-8fb9-dd769a06f13b.pdf',
+		},
+	]
 </script>
 
 <div
@@ -51,6 +67,40 @@
 				<span class="font-medium">Intern kommentar</span>
 			</div>
 			<p class="whitespace-pre-line text-sm">{log.internal_comment}</p>
+		</div>
+	{/if}
+	{#if internal && attachments.length > 0}
+		<div class="mt-3 flex items-center gap-2">
+			{#each attachments as att (att.id)}
+				<Tooltip.Provider delayDuration={250}>
+					<Tooltip.Root>
+						<Tooltip.Trigger>
+							<div
+								class="group flex size-12 cursor-pointer items-center justify-center overflow-hidden rounded-lg border"
+							>
+								{#if att.type == 'document'}
+									<a
+										href={att.url}
+										target="_blank"
+										class="grid h-full w-full place-items-center bg-muted/40 transition-colors group-hover:bg-muted/100"
+									>
+										<FileText class="size-5 fill-slate-200 text-slate-500" />
+									</a>
+								{:else if att.type == 'image'}
+									<img
+										alt={att.filename}
+										src={att.url}
+										class="transition-transform group-hover:scale-105"
+									/>
+								{/if}
+							</div>
+						</Tooltip.Trigger>
+						<Tooltip.Content sideOffset={8}>
+							<div class="">{att.filename}</div>
+						</Tooltip.Content>
+					</Tooltip.Root>
+				</Tooltip.Provider>
+			{/each}
 		</div>
 	{/if}
 </div>
